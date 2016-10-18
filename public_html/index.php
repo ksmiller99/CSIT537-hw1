29a1d7b37db2222386ae9ddf7dc3cd58be036e64
Kevin S. Miller <ksmiller99@gmail.com>
2016-10-18 11:49:52 -0400

    Changed project type to PHP

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
-
+<?php
+try{
+    $pdo = new PDO('mysql:host=localhost;dbname=csit537_project1', 'WA_DbUser', 'secretpassword1');
+}catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
 <html>
     <head>
         <title>2017 World Congress CS-IT Conferences - Home</title>
